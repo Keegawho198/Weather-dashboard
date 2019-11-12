8aabc9872bae4a61f70b48281f60c5ec24ae46cd
Keegan Nair <keegan.nair@hotmail.com>
2019-11-12 19:42:43 +1100

    Added added more connections to api for temp and so on. need to check how to format the date properly

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 var APIKEY = "b15f97422ba66d215ee17499ebc5b83b";
 
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=london&units=imperial&APPID=" + APIKEY;
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=melbourne&units=imperial&APPID=" + APIKEY;
 
 //use AJAX to call weather api
 $.ajax({
@@ -15,43 +15,39 @@ $.ajax({
 
     console.log(response);
 
+    var today = new Date();
+    var dd = today.getDate();
+    var dd1 = today.getDate()+1; //for next day after current day
+    var dd2 = today.getDate()+2;
+    var dd3 = today.getDate()+3;
+    var dd4 = today.getDate()+4;
+    var dd5 = today.getDate()+5;
+
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+
     // var iconcode = a.weather[0].icon;
     // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
     // $('#wicon').attr('src', iconurl);
     // " + response.list[0].weather[0].icon +"
 
-  $(".cityName").html("<h1>" + response.city.name + " Weather Details </h1>");
-  $(".cityTemp").html("<p> Temperature (F): " + response.list[0].main.temp)
-  $(".cityHumid").html("<p> Humidity: " + response.list[0].main.humidity);
-  $(".cityWind").html("<p> Wind Speed: " + response.list[0].wind.speed + " MPH</p>")
+    //main data for the main city
+    //$(".cityName").html("<h1>" + response.city.name + " Weather Details (" + dd + "/" + mm + "/" + yyyy + ")</h1>");
+    $(".cityName").html("<h1>" + response.city.name + response.list[0].dt_txt);
+
+    $(".cityTemp").html("<p> Temperature (F): " + response.list[0].main.temp +"°")
+    $(".cityHumid").html("<p> Humidity: " + response.list[0].main.humidity + "%");
+    $(".cityWind").html("<p> Wind Speed: " + response.list[0].wind.speed + " MPH</p>")
+
+
+    //dates for the five day forecase
+    $("#day1").html("<h5>" + dd1 + "/" + mm + "/" + yyyy);
+    $("#firstDayTemp").html("<p> Temperature (F): " + response.list[1].main.temp + "° <p/>");
+    $("#firstDayHumid").html("<p> Humidity: " + response.list[1].main.temp + "%");
+
   });
 
-    // // Here we run our AJAX call to the OpenWeatherMap API
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET"
-    // })
-    //   // We store all of the retrieved data inside of an object called "response"
-    //   .then(function(response) {
-
-    //     // Log the queryURL
-    //     console.log(queryURL);
-
-    //     // Log the resulting object
-    //     console.log(response);
-
-    //     // Transfer content to HTML
-    //     $(".cityWeather").html("<h1>" + response.name + " Weather Details</h1>");
-    //     // $(".wind").text("Wind Speed: " + response.wind.speed);
-    //     // $(".humidity").text("Humidity: " + response.main.humidity);
-    //     // $(".temp").text("Temperature (F) " + response.main.temp);
-
-    //     // Converts the temp to Kelvin with the below formula
-    //     // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-    //     // $(".tempF").text("Temperature (Kelvin) " + tempF);
-
-    //     // Log the data in the console as well
-    //     // console.log("Wind Speed: " + response.wind.speed);
-    //     // console.log("Humidity: " + response.main.humidity);
-    //     // console.log("Temperature (F): " + response.main.temp);
-    //   });
+  //questions to ask
+  // how do we get the date, cuz when i try to use the 5 day forecast, it does it for every 3 hours
+  // do we still use that or do we need to use something else?
+  //
