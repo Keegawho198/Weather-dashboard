b47e7c9f4279cdf40faaa568e80420cddbf74923
Keegan Nair <keegan.nair@hotmail.com>
2019-12-02 14:18:13 +1100

    deleted comments that are not used

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//searchCity = $("#savedSearch").val();
-
-//var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=melbourne&units=imperial&APPID=" + APIKEY;
+//makes sure that the previous searchs are not repeated
 $("#cityHistory").empty();
 displayCity();
 function displayCity() {
@@ -38,14 +36,11 @@ $("#searchBtnId").click(function () {
       localStorage.setItem("storedCity", JSON.stringify(storedCity));
       $("savedSearch").val("");
     }
-    //makes sure same text is not repeated when displayed
     $("#cityHistory").append('<li>' + city + '</li>');
     
   } else {
     alert("enter something")
   }
-
-  
 
 
   //use AJAX to call weather api
@@ -81,22 +76,17 @@ $("#searchBtnId").click(function () {
 
       var iconCode = response.list[1].weather[0].icon;
 
-      //var iconCode = "09d";
       console.log(iconCode);
       var iconImg = "http:openweathermap.org/img/wn/" + iconCode + ".png";
 
-      //console.log(response.city.list[1].weather[0].icon); //console log this and get 504 error
-
-
       //main data for the main city
-      //$(".cityName").html("<h1>" + response.city.name + " Weather Details (" + dd + "/" + mm + "/" + yyyy + ")</h1>");
       $(".cityName").html("<h1>" + response.city.name + " " + dT + "<img src =" + iconImg + ">");
 
       $(".cityTemp").html("<p> Temperature (F): " + response.list[1].main.temp + "°")
       $(".cityHumid").html("<p> Humidity: " + response.list[1].main.humidity + "%");
       $(".cityWind").html("<p> Wind Speed: " + response.list[1].wind.speed + " MPH</p>")
 
-      //dates for the five day forecase
+      //dates for the five day forecast
       //each list array is for the different days as this api does the weather for every 3 hours
       //in this case every day is for every 12pm
       var iconCode1 = response.list[9].weather[0].icon;
@@ -139,7 +129,6 @@ $("#searchBtnId").click(function () {
 });
 
 var APIKEY = "b15f97422ba66d215ee17499ebc5b83b";
-
 
 var queryURL_UV = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKEY + "&lat=-37.8143&lon=144.9632";
 
